ab050ec2ab1f382a3b2dbe6126bf29c2a82c50d9
RahanBenabid <rahannadime@gmail.com>
2025-03-29 13:27:27 +0100

    Initialize auth service and add gitignore

---
+++ python/src/auth/server.py
@@ -0,0 +1,20 @@
+import jwt, datetime, os
+from flask import Flask, request
+from flask_mysqldb import MySQL
+
+# this will act as a registry for the view functions, the url rules and much mor (it will configure the object)
+server = Flask(__name__)
+
+# connect and query the database
+mysql = MySQL(server)
+
+server.config["MYSQL_HOST"] = os.environ.get("MYSQL_HOST")
+server.config["MYSQL_USER"] = os.environ.get("MYSQL_USER")
+server.config["MYSQL_PASSWORD"] = os.environ.get("MYSQL_PASSWORD")
+server.config["MYSQL_DB"] = os.environ.get("MYSQL_DB")
+server.config["MYSQL_PORT"] = os.environ.get("MYSQL_PORT")
+
+
+@server.route("/login", methods=['POST'])
+def login():
+	auth = request.authorization
