27fed3b7477b25f59b7024328a76d84e0a31a5a4
RahanBenabid <rahannadime@gmail.com>
2025-03-30 07:42:57 +0100

    Add auth login endpoint and database setup

--- python/src/auth/server.py
+++ python/src/auth/server.py
@@ -18,3 +18,24 @@ server.config["MYSQL_PORT"] = os.environ.get("MYSQL_PORT")
 @server.route("/login", methods=['POST'])
 def login():
 	auth = request.authorization
+	if not auth:
+		return "missing credentials", 401
+	
+	# check db for username and pw
+	cur = mysql.connection.cursor()
+	res = cur.execute(
+		"SELECT email, password FROM user WEHRE email=%s", (auth.username,)
+	)
+	
+	if res > 0:
+		user_row = cur.fetchone()
+		email = user_row[0]
+		password = user_row[1]
+		
+		if auth.username != email or auth.password != password:
+			return "invalid credentials", 401
+		else:
+			return createJWT(auth.username, os.environ.get("JWT_SECRET"))
+	else:
+		return "invalid credentials", 401
+
